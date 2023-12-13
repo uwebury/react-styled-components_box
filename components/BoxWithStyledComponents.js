8c68ac805a83c2751e834fd89399e7456d9a18d0
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-13 16:00:39 +0100

    Refactored code

--- components/BoxWithStyledComponents.js
+++ components/BoxWithStyledComponents.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 // Styled component for the box
-const StyledBox = styled.div`
+export default styled.div`
   width: 100px;
   height: 100px;
   background-color: ${(props) => (props.$isBlack ? "black" : "green")};
@@ -11,8 +11,3 @@ const StyledBox = styled.div`
     background-color: red;
   }
 `;
-
-// Function component that uses StyledBox
-export default function BoxWithStyledComponents({ $isBlack }) {
-  return <StyledBox $isBlack={$isBlack} />;
-}
