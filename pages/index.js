23d1158e2efb050c3f64de8b531f6f6dad72b784
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-13 15:51:22 +0100

    Aligned boxes horizontically

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
+import styled from "styled-components";
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "@/components/BoxWithStyledComponents.js";
 
 export default function HomePage() {
   return (
-    <div>
+    <StyledWrapper>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-    </div>
+      <BoxWithStyledComponents />
+      <BoxWithStyledComponents $isBlack />
+    </StyledWrapper>
   );
 }
+
+const StyledWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
